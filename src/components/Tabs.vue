041230b3dcf5b91f15ceeebcc7a41b82026c2f0b
gingerlittlefish <136437988+gingerlittlefish@users.noreply.github.com>
2024-04-23 23:38:16 +0800

    fix:tab选中样式失效

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -3,7 +3,7 @@
       <ul class="flex">
         <li v-for="tab in tabs" :key="tab.id" class="mr-4">
           <a href="#" @click.prevent="selectTab(tab.id)"
-            :class="['px-4 py-2 rounded-md shadow-md hover:bg-blue-300', modelValue === tab.id ? 'bg-blue-500 text-white' : 'bg-white text-black']">
+            :class="['px-4 py-2 rounded-md shadow-md hover:bg-blue-300', selectedTab === tab.id ? 'bg-blue-500 text-white' : 'bg-white text-black']">
             {{ tab.label }}
           </a>
         </li>
