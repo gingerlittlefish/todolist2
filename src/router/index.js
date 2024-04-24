189e0f7a3f022d26ba411beff7dda9860530937e
gingerlittlefish <136437988+gingerlittlefish@users.noreply.github.com>
2024-04-24 23:46:15 +0800

    重命名

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ArticleList from '../components/ArticleList.vue'
-import ArticleDetail from '../views/ArticleDetail.vue' 
+import ArticleDetails from '../views/ArticleDetails.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,7 +19,7 @@ const router = createRouter({
     {
       path: '/article/:id',
       name: 'articleDetail',
-      component: ArticleDetail,
+      component: ArticleDetails,
     },
   ],
 })
